76752caf87cbdc224ee3b3aa3c3d822c7b7356c1
kawa <kawa2287@gmail.com>
2018-02-27 22:13:13 +0000

    attempt at adjusting the layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,23 +9,13 @@ export default class Layout extends React.Component {
       <div className="app-container">
         <header>
           <Link to="/">
-            <img className="logo" src="/img/logo-judo-heroes.png"/>
+            <img className="logo" src="/img/headers/bracketTitle.png"/>
           </Link>
         </header>
         <div className="app-content">{this.props.children}</div>
         <footer>
           <p>
-            This is a demo app to showcase universal rendering and routing with <strong>React</strong> and <strong>Express</strong>.
-          </p>
-
-          <p>
-            Do you want to know more about Node.js and Universal JavaScript? <strong>Checkout <a href="https://nodejsdesignpatterns.com">Node.js Design Patterns</a></strong>.
-          </p>
-
-          <p>
-            Built with <strong>❤</strong>︎ and <strong>code</strong> by <a href="http://loige.co" target="_blank">loige</a>.
-            Contribute on <a href="https://github.com/lmammino/judo-heroes">GitHub</a> or
-            read the tutorial on <a href="https://scotch.io/tutorials/react-on-the-server-for-beginners-build-a-universal-react-and-node-app">Scotch.io</a>
+            This is a test app for a web based double elimination bracket.
           </p>
         </footer>
       </div>
