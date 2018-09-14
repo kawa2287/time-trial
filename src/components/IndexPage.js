f6c7cdeb449dbfce5882628ebffdb3b2ea4751bd
kawa <kawa2287@gmail.com>
2018-09-14 17:54:58 +0000

    another upload

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -128,7 +128,7 @@ export default class IndexPage extends React.Component {
 		
   		//this section is to populate random number of teams... delete for production use
 	  	//---------------------------------------------------------------------------------
-	  	var nTeams = 15;
+	  	var nTeams = 8;
 	  	
 	  	var countryArr = [];
 	  	for (var item in countries[0]){
